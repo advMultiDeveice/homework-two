bd22b000ac1dca7931af32d598b609c6274d252a
jroper7 <josephroper57@gmail.com>
2024-09-22 21:28:50 -0400

    Added README.md

--- app/app.js
+++ app/app.js
@@ -1,12 +1,9 @@
 import {loadPage } from "../model/model.js";
 
-
 function changeRoute() {
 let hashTag = window.location.hash;
 let pageID = hashTag.replace('#', '');
-
     loadPage(pageID);
-
 
 }
 
